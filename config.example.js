7cc81b3844b30a336debcbea4b17c8b4517f6e8a
Tejveer Singh <tejveer.bath@kin.com>
2026-01-06 16:53:50 -0700

    Added firebase, add and fetch functionality

---
+++ config.example.js
@@ -0,0 +1,4 @@
+// Copy this file to config.js and add your actual Firebase URL
+const BACKEND_URL = 'YOUR_FIREBASE_URL_HERE';
+
+export { BACKEND_URL };
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/style';
 import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
+import { storeExpense } from '../util/http';
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -33,6 +34,7 @@ function ManageExpense({ route, navigation }) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
+      storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
